069f2cb8bb36264c79d4090afe16aa4edac22cea
SimonMarcelo <marcelo.simon@gmail.com>
2022-09-26 21:44:31 -0300

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React from "react";
-
-import Ejercicio3 from "./Ejercicios/Piedra-Papel-Tijera";
+import Efectos from "./efectos/Efectos";
+import Ejercicio1 from "./Ejercicios/Ejercicio1";
+import Ejercicio2 from "./Ejercicios/Ejercicio2";
+import PiedraPapelTijeraV2 from "./Ejercicios/PiedraPapelTijeraV2";
+import Correcciones from "./estados/Correcciones";
+import Estados from "./estados/Estados";
+import Llamadas from "./llamadas/Llamadas";
+import LlamadasSr from "./llamadas/LlamadasSr";
+import Promesas from "./promesas/Promesas";
+import Card from "./props/Card/Card";
+import ComponenteA from "./props/ComponenteA";
 
 const App = () => {
   return (
     <>
-      <Ejercicio3/>
+      <Card />
+      <Card />
     </>
   );
 };
